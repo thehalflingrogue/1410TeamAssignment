18c8cad8c25dde5a585505bbd1fbd98c69fb5d87
RafaelV05 <69766499+RafaelV05@users.noreply.github.com>
2022-04-15 12:03:21 -0600

    Update Player.java

--- bangDice/Player.java
+++ bangDice/Player.java
@@ -2,7 +2,31 @@ package bangDice;
 
 public class Player
 {
-    public Player() {
+	private String playerName = "";
 
+//CPU - no name provided
+    public Player(Characters charData, Role playerRole) 
+    {
+    	this.playerName = "CPU";
+    
     }
+//Human
+    public Player(Characters charData, Role playerRole, String playerName) 
+    {
+    	this.playerName = playerName;
+    
+    }
+
+
+	public String getPlayerName()
+	{
+		return playerName;
+	}
+// the main purpose of the setter is for the CPU player to be labeled with a number like CPU 1, CPU 2.. 
+	public void setPlayerName(String playerName)
+	{
+		this.playerName = playerName;
+	}
+    
+    
 }
