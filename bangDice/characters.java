e991128466b03efcb7d43aa682bbc8aeb8c535c0
Ben Warner <thehalflingrogue@gmail.com>
2022-04-14 20:27:20 -0600

    Updated Characters class  - String Array of 16 character names  - Randomizer to pick one name in array  - Constrctor that will set passed String to the random name

--- bangDice/characters.java
+++ bangDice/characters.java
@@ -1,8 +1,19 @@
 package bangDice;
 
-public class characters
+import java.util.Random;
+
+public class Characters
 {
-    public static void main(String[] args) {
-        System.out.println("Testing");
+    public Random rand = new Random();
+    int randomName = rand.nextInt(16);
+
+    String[] characterName = {
+            "Paul Regret", "Willy the Kid", "Black Jack", "Calamity Janet", "Pedro Ramirez",
+            "Jourdonnais", "Lucky Duke", "Jesse Jones", "Sid Ketchum", "Rose Doolan", "El Gringo",
+            "Slab the Killer", "Kit Carlson", "Vulture Sam", "Bart Cassidy", "Suzy Lafayette"};
+
+    public Characters(String playerCharacter)
+    {
+        playerCharacter = characterName[randomName];
     }
 }
