282277d69905c972101e349c893f0cf75744285a
Ben Warner <thehalflingrogue@gmail.com>
2022-04-16 10:59:26 -0600

    Changed Dice class and takeTurn() method

--- bangDice/Bang.java
+++ bangDice/Bang.java
@@ -15,9 +15,15 @@ public class Bang {
         Scanner kB = new Scanner( System.in );
 
         int arrowPile = 9;
-        Dice[] dice = new Dice[6];
-        ArrayList<Player> players = new ArrayList<>();
 
+        Dice[] dice = new Dice[6];
+        for (int i = 0; i < 6; i++) {
+            Dice newDie = new Dice();
+            dice[i] = newDie;
+        }
+
+
+        ArrayList<Player> players = new ArrayList<>();
 
         //Assuming 4 player game. Will need to change this block if we add other game modes
         ArrayList<Role> roles = new ArrayList<>();
@@ -35,24 +41,51 @@ public class Bang {
 
         createPlayers(kB, players, roles, numPlayers, ranInt); //test method - will need changes once GUI is set up
 
-        while(endGame==false)
+        /**
+         * Will Be the takeTurn() method - will refactor after it is working
+         */
+        Integer[] endRoll = new Integer[6];
+        endRoll[0]=7;
+        if(currentPlayer >= 4)
         {
-            if(currentPlayer >= 4)
-            {
-                currentPlayer=0;
-            }
-            System.out.println(players.get(currentPlayer) + ", It is your turn. \n");
-
-            System.out.println(dice);
-
-            for (int d=0; d<6;d++)
-            {
-                dice[d].Roll();
-                System.out.println(dice[d]);
-            }
-
-
+            currentPlayer=0;
         }
+        System.out.println(players.get(currentPlayer) + ", It is your turn. \n");
+
+        for (int d=0; d<6;d++)
+        {
+            dice[d].Roll();
+            System.out.print("Dice #"+d+":"+dice[d]+"  ");
+        }
+
+        String line;
+        do
+        {
+
+            System.out.println("Which dice would you like to re-roll? \nPlease enter with a space between each number or 0 for none");
+            line = kB.next();
+
+            String[] num = line.split(" ");
+            for (int i = 0; i < num.length; i++)
+            {
+                endRoll[i] = Integer.parseInt(num[i]);
+            }
+
+
+            if (endRoll[0] != 0)
+            {
+                for (int i = 0; i < num.length; i++)
+                {
+                    dice[endRoll[i]].Roll();
+                    System.out.print("Dice #" + (i+1) + ":" + dice[i] + "  ");
+                }
+
+            }
+        }while(endRoll[0]!=0);
+
+
+
+
 
 
 
--- bangDice/Dice.java
+++ bangDice/Dice.java
@@ -12,39 +12,61 @@ import java.util.Random;
 public class Dice {
 	
 	Random ranNum = new Random();
+
 	ArrayList<Integer> diceKeptArrayList = new ArrayList<>();
+
 	int randomNumber = 0;
 	int count =0;
-	String faces[] = {"Arrow", "Dynamite", "Beer", " Bulls Eye 1", "Bulls Eye 2","Gatling"};
-	/*String currentFace;
 
-	public Dice() {
-		currentFace = faces[ranNum.nextInt(6)];
-	}*/
+	String faces[] = {"Arrow", "Dynamite", "Beer", "Bullseye-1", "Bullseye-2","Gatling"};
+	String currentFace;
 
+	public Dice()
+	{
+		currentFace= faces[ranNum.nextInt(6)];
+	}
 
-
-	public void Roll() {
-
-		for (int i = 0; i < 5; i++) {
+	public void Roll()
+	{
+		if(count==0)
+		{
+			currentFace = faces[ranNum.nextInt(6)];
+		}
+		else if(count<=3)
+		{
+			if(currentFace.equals("Dynamite"))
+			{
+				System.out.println("Cannot re-roll Dynamite");
+			}
+			else
+			{
+				currentFace= faces[ranNum.nextInt(6)];
+			}
+		}
+		else
+		{
+			System.out.println("Cannot roll more than 3 times");
+		}
+		/*for (int i = 0; i < 5; i++) {
 
 			randomNumber = ranNum.nextInt(6);
 			diceKeptArrayList.add(randomNumber);
 //			System.out.print(randomNumber);
 //			System.out.print(" ");
-		}
+		}*/
 
 		
 
 	}
 	
-	public void ReRoll() {
-		
-		for(int i = 0; i<5-diceKeptArrayList.size();i++) {
+	public void ReRoll()
+	{
+
+		/*for(int i = 0; i<5-diceKeptArrayList.size();i++) {
 			randomNumber = ranNum.nextInt(6);
 			diceKeptArrayList.add(randomNumber);
 
-		}
+		}*/
 		
 	}
 
@@ -58,20 +80,32 @@ public class Dice {
 		count++;	
 	}
 
+	public String getCurrentFace() {
+		return currentFace;
+	}
+
+	public void setCurrentFace(String currentFace) {
+		this.currentFace = currentFace;
+	}
 
 	@Override
+	public String toString() {
+		return currentFace;
+	}
+
+	/*@Override
 	public String toString() {
 		String diceString = "";
 
 	 for (int i: diceKeptArrayList) {
-		 diceString += faces[i] + " ";
+		 diceString += faces[i] + ", ";
 
 
 	 }
 
 		return diceString;
 
-	}
+	}*/
 
 }
 
